17bd21710db48155c0654f800c6dda31cc4ce606
ayxan65 <memmedliayxan0@gmail.com>
2024-05-21 21:36:46 +0400

    added

--- my-app/app/Home/Header.tsx
+++ my-app/app/Home/Header.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
+import '../css/home.css';
+import 
 
 
 const Header = () => {
   return (
  
     <div className='  h-full mx-7 py-2 '>
-        <div className='flex justify-between'>
+        <div className=' mt-16 flex justify-between'>
             <div className='flex flex-col'><span className='text-stone-600  text-6xl'>Let's Ride</span>
                 <span className='font-bold text-8xl text-stone-700'>The Future</span>
             </div>
         </div>
-
+        <div className='w '></div>
+        <div className='text-stone-800 mt-14 text-xl '><p className='w-64'>simple and sleek desing users in mind</p></div>
+        <div className='mt-16'>Buy now</div>
     </div>
   )
 }
--- my-app/app/Home/NavBar.tsx
+++ my-app/app/Home/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     <div className='border h-full mx-7 py-2'>
       <div className='flex justify-between items-center gap-12 text-stone-700 py-4 mx-4'>
         <Image src={Logo} alt='photo'></Image>
-        <div className='flex gap-12'>
+        <div className='flex gap-12 '>
           <div className='cursor-pointer py-2'>Product</div>
           <div className='cursor-pointer py-2'>Gallery</div>
           <div className='cursor-pointer py-2'>Contact</div>
@@ -17,9 +17,7 @@ const NavBar = () => {
         <div className='cursor-pointer'>
           <button className='mx-6'>Log in</button>
           <button 
-            onClick={() => {
-              alert("salam olsun");
-            }} 
+            
             className='bg-stone-700 text-white rounded-lg py-2 px-4 cursor-pointer'
           >
             Sign up
