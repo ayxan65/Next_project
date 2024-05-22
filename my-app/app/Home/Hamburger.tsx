bcc0dcb6e3cf11fdb2c57e1c83623c8fa0872825
ayxan65 <memmedliayxan0@gmail.com>
2024-05-22 11:14:55 +0400

    added

--- my-app/app/Home/Hamburger.tsx
+++ my-app/app/Home/Hamburger.tsx
@@ -3,18 +3,32 @@ import React, { useState } from 'react';
 import { FaHamburger } from 'react-icons/fa';
 
 const Hamburger = () => {
- 
   const [isVisible, setIsVisible] = useState(false);
 
-  
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   };
 
   return (
-    <div className='sm:block md:block lg:hidden'>
-      <div className={isVisible ? 'block' : 'hidden'}>
-        salam olsun azerbaycan esgerine
+    <div className='relative sm:block md:block lg:hidden'>
+      <div
+        className={`cursor-auto my-5 flex flex-col items-center fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ${
+          isVisible ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <div className='cursor-pointer py-3'>Product</div>
+        <div className='cursor-pointer py-3'>Gallery</div>
+        <div className='cursor-pointer py-3'>Contact</div>
+        <div className='cursor-pointer py-3 flex flex-col items-center'>
+        <div className='my-5 '>
+        <button className='mx-6 mb-3 text-black'>Log in</button>
+          <button 
+            className='bg-stone-700 text-white rounded-lg py-2 px-4'
+          >
+            Sign up
+          </button>
+        </div>
+        </div>
       </div>
 
       <button onClick={toggleVisibility} className='m-7'>
